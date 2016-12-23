47f19c7060b1c05180a1999b5fea1adf7e0ab9f2
Berry Firmann <clickdisini@gmail.com>
2016-12-23 12:56:52 +0700

    base64 image method

--- app/Modules/Post/Http/Controllers/PostController.php
+++ app/Modules/Post/Http/Controllers/PostController.php
@@ -32,7 +32,7 @@ class PostController extends Controller
             $category       = $request->category;
             $filter         = $request->filter;
             $sort           = $request->sort;
-            
+
             $post = Post::with('category','writer','editor','admin')->filter($filter);
             if ($q!=null) {
                 $post->search($q,$result_type);
@@ -56,7 +56,7 @@ class PostController extends Controller
             $meta['prev']=$post->previousPageUrl();
             $meta['from'] = $post->firstItem();
             $meta['to'] = $post->lastItem();
-            $data = $post->all();    
+            $data = $post->all();
         }
         else
         {
@@ -65,7 +65,7 @@ class PostController extends Controller
             $meta['error'] = $validator->errors();
             $data = null;
         }
-        
+
         $meta['code'] = 200;
         $code = 200;
         return response()->json(compact('meta','data'),$code);
@@ -93,7 +93,7 @@ class PostController extends Controller
         $validator = \Validator::make($request->all(), [
                 'title' => 'required|string',
                 'category_id' => 'integer',
-                'image' => 'image',
+                'image' => 'string',
                 'article' => 'required|string|min:3',
                 'writer_id' => 'required|integer',
                 'admin_id' => 'integer',
@@ -107,22 +107,29 @@ class PostController extends Controller
             $writer_id  = $request->writer_id;
             $admin_id   = $request->admin_id;
             $status     = 0;
-            $image      = $request->file('image');
+            $image      = $request->input('image');
             $type       = $request->type;
             $url        = $request->url;
-            
-            try 
+
+            try
             {
                 //upload image to storage
                 if ($image!= null && $type == 'image') {
-                    $extension  = $image->getClientOriginalExtension();
+                    $image = \Image::make($image);
+                    if ($image->mime() == 'image/jpeg')
+				                $extension = '.jpg';
+				            elseif ($image->mime() == 'image/png')
+				                $extension = '.png';
+				            elseif ($image->mime() == 'image/gif')
+				                $extension = '.gif';
+                    // $extension  = $image->getClientOriginalExtension();
                     $fileName      = str_replace(' ', '_', $title).uniqid();
                     $mergeFileName = $fileName.'.'.$extension;
                     $destination   = storage_path('app/public/');
-                    
-                    $image = \Storage::put(
+
+                    $image_save = \Storage::put(
                         'public/'.$mergeFileName,
-                        file_get_contents($request->file('image')->getRealPath())
+                        $image->stream()
                     );
                     //resize image
                     $resize = \Image::make(asset('storage/'.$mergeFileName));
@@ -137,21 +144,21 @@ class PostController extends Controller
                     $resize->fit(280, 140)->save($destination . $fileName . '_square280.' . $extension);
 
                 } else if ($type == 'video' && $url !== null) {
-                     
+
                     $content = $this->getMediaInfo($url, $type);
-                    
-                    if ($content !== false) 
+
+                    if ($content !== false)
                     {
                         $content = json_encode($content);
                     }
                     else
                     {
-                        $content = null;         
+                        $content = null;
                     }
                 } else {
                     $content = null;
                 }
-                
+
                 $post = new Post;
                 $post->title        = $title;
                 $post->category_id  = $category_id;
@@ -159,7 +166,7 @@ class PostController extends Controller
                 $post->status       = 0;
                 if (isset($mergeFileName)) {
                     $mergeFileName  = ['file' => $mergeFileName];
-                    $post->content  = json_encode($mergeFileName);   
+                    $post->content  = json_encode($mergeFileName);
                 } else if ($content!= null) {
                     $post->content  = $content;
                 }
@@ -167,12 +174,12 @@ class PostController extends Controller
                 $post->writer_id    = $writer_id;
                 if ($post->save()) {
                     $meta['status'] = true;
-                    $meta['message'] = "Success creating post";    
+                    $meta['message'] = "Success creating post";
                     $data = $post;
                 }
-                
-            } 
-            catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) 
+
+            }
+            catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e)
             {
                 $meta['status'] = false;
                 $meta['message'] = 'Error '.$e;
@@ -190,7 +197,7 @@ class PostController extends Controller
             $meta['error'] = $validator->errors();
             $data = null;
         }
-        
+
         $meta['code'] = 200;
         $code = 200;
         return response()->json(compact('meta','data'),$code);
@@ -205,28 +212,28 @@ class PostController extends Controller
      */
     public function show(Request $request)
     {
-        
+
         $validator = \Validator::make($request->all(), [
                 'id'       => 'required',
         ]);
         if ($validator->passes()) {
             $id        = $request->id;
 
-            try 
+            try
             {
                 if (is_numeric($id)) {
                     $post = Post::with('category','writer','editor','admin')->findOrFail($id);
                     $meta['status'] = true;
                     $meta['message'] = "Success showing post ID #".$id;
-                    $data = $post;       
+                    $data = $post;
                 } else if (is_string($id)) {
                     $post = Post::with('category','writer','editor','admin')->findBySlug($id);
                     $meta['status'] = true;
                     $meta['message'] = "Success showing post";
                     $data = $post;
-                }                 
-            } 
-            catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) 
+                }
+            }
+            catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e)
             {
                 $meta['status'] = false;
                 $meta['message'] = 'Error '.$e;
@@ -244,12 +251,12 @@ class PostController extends Controller
             $meta['error'] = $validator->errors();
             $data = null;
         }
-        
+
         $meta['code'] = 200;
         $code = 200;
         return response()->json(compact('meta','data'),$code);
         // return $this->response->array(compact('meta','data'))->setStatusCode($code);
-    
+
     }
 
     /**
@@ -291,8 +298,8 @@ class PostController extends Controller
             $admin_id   = $request->admin_id;
             $status     = $request->status;
             $image      = $request->file('image');
-            
-            try 
+
+            try
             {
                 //upload image to storage
                 $extension  = $image->getClientOriginalExtension();
@@ -319,17 +326,17 @@ class PostController extends Controller
                 $post->article      = $article;
                 $post->status       = 0;
                 if (isset($mergeFileName)) {
-                    $post->image    = $mergeFileName;   
+                    $post->image    = $mergeFileName;
                 }
                 $post->writer_id    = $writer_id;
                 if ($post->save()) {
                     $meta['status'] = true;
-                    $meta['message'] = "Success updating post";  
-                    $data = $post;  
+                    $meta['message'] = "Success updating post";
+                    $data = $post;
                 }
-                
-            } 
-            catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) 
+
+            }
+            catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e)
             {
                 $meta['status'] = false;
                 $meta['message'] = 'Error '.$e;
@@ -347,7 +354,7 @@ class PostController extends Controller
             $meta['error'] = $validator->errors();
             $data = null;
         }
-        
+
         $meta['code'] = 200;
         $code = 200;
         return response()->json(compact('meta','data'),$code);
@@ -367,7 +374,7 @@ class PostController extends Controller
             $admin_id = $request->admin_id;
             $editor_id = $request->editor_id;
             $status   = $request->status;
-            try 
+            try
             {
                 $post = Post::findOrFail($id);
                 $post->status = $status;
@@ -376,8 +383,8 @@ class PostController extends Controller
                     $meta['message'] = 'Success Update status';
                     $data  = $post;
                 }
-            } 
-            catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) 
+            }
+            catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e)
             {
                 $meta['status'] = false;
                 $meta['message'] = 'Error '.$e;
@@ -386,7 +393,7 @@ class PostController extends Controller
                 $meta['status'] = false;
                 $meta['message'] = 'Error '.$e;
             }
-            
+
         }
         else
         {
@@ -395,7 +402,7 @@ class PostController extends Controller
             $meta['error'] = $validator->errors();
             $data = null;
         }
-        
+
         $meta['code'] = 200;
         $code = 200;
         return response()->json(compact('meta','data'),$code);
@@ -409,15 +416,15 @@ class PostController extends Controller
      */
     public function destroy($id)
     {
-        try 
+        try
         {
             $post = Post::findOrFail($id);
             if ($post->delete()) {
                 $meta['status'] = true;
-                $meta['message'] = "Success deleting post"; 
+                $meta['message'] = "Success deleting post";
             }
-        } 
-        catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) 
+        }
+        catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e)
         {
             $meta['status'] = false;
             $meta['message'] = 'Error '.$e;
@@ -426,7 +433,7 @@ class PostController extends Controller
             $meta['status'] = false;
             $meta['message'] = 'Error '.$e;
         }
-                    
+
         $meta['code'] = 200;
         $code = 200;
         return response()->json(compact('meta','data'),$code);
@@ -447,18 +454,18 @@ class PostController extends Controller
             $count = $count->count();
             if ($count >= 5) {
                 $meta['status'] = true;
-                $meta['message'] = "Featured Post is Full";       
+                $meta['message'] = "Featured Post is Full";
             } else {
-                try 
+                try
                 {
                     $post = Post::findOrFail($id);
                     $post->featured = 1;
                     if ($post->save()) {
                         $meta['status'] = true;
-                        $meta['message'] = 'Success creating featured post';       
+                        $meta['message'] = 'Success creating featured post';
                     }
-                } 
-                catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) 
+                }
+                catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e)
                 {
                     $meta['status'] = false;
                     $meta['message'] = 'Error '.$e;
@@ -469,7 +476,7 @@ class PostController extends Controller
                 }
 
             }
-        
+
         }
         else
         {
@@ -478,7 +485,7 @@ class PostController extends Controller
             $meta['error'] = $validator->errors();
             $data = null;
         }
-        
+
         $meta['code'] = 200;
         $code = 200;
         return response()->json(compact('meta','data'),$code);
@@ -494,16 +501,16 @@ class PostController extends Controller
         if ($validator->passes()) {
             $id = $request->id;
             $post_id = $request->post_id;
-            try 
+            try
             {
                 $post = Post::findOrFail($id);
                 $post->featured = 0;
                 if ($post->save()) {
                     $meta['status'] = true;
-                    $meta['message'] = 'Success unset featured post';       
+                    $meta['message'] = 'Success unset featured post';
                 }
-            } 
-            catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) 
+            }
+            catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e)
             {
                 $meta['status'] = false;
                 $meta['message'] = 'Error '.$e;
@@ -511,7 +518,7 @@ class PostController extends Controller
             catch (\Illuminate\Database\QueryException $e) {
                 $meta['status'] = false;
                 $meta['message'] = 'Error '.$e;
-            }        
+            }
         }
         else
         {
@@ -520,7 +527,7 @@ class PostController extends Controller
             $meta['error'] = $validator->errors();
             $data = null;
         }
-        
+
         $meta['code'] = 200;
         $code = 200;
         return response()->json(compact('meta','data'),$code);
@@ -529,16 +536,16 @@ class PostController extends Controller
 
     public function getFeatured()
     {
-        try 
+        try
         {
-            $featured = Post::with('admin','writer','editor','category')->Featured(); 
+            $featured = Post::with('admin','writer','editor','category')->Featured();
 
             $meta['status'] = true;
             $meta['message'] = "Showing all featured post";
             // $meta['error'] = $validator->errors();
             $data = $featured;
-        } 
-        catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e) 
+        }
+        catch (\Illuminate\Database\Eloquent\ModelNotFoundException $e)
         {
             $meta['status'] = false;
             $meta['message'] = 'Error '.$e;
@@ -547,7 +554,7 @@ class PostController extends Controller
             $meta['status'] = false;
             $meta['message'] = 'Error '.$e;
         }
-               
+
         $meta['code'] = 200;
         $code = 200;
         return response()->json(compact('meta','data'),$code);
@@ -556,8 +563,8 @@ class PostController extends Controller
 
     protected function getMediaInfo($mediaUrl, $type) {
         $mediaUrl = trim($mediaUrl);
-        
-        
+
+
         if($type == 'video') {
             preg_match('~(?#!js YouTubeId Rev:20160125_1800)
                 # Match non-linked youtube URL in the wild. (Rev:20130823)
@@ -609,7 +616,7 @@ class PostController extends Controller
                 [?=&+%\w.-]*       # Consume any URL (query) remainder.
                 ~ix', '$1',
                 $mediaUrl);
-                
+
                 $apiPublicKey = 'AIzaSyBjID4YyCdhd1nJK1R8eFIHBSkFecjY0_w';
                 $checkUrl = 'https://www.googleapis.com/youtube/v3/videos?part=id&id=' . $videoId . '&key=' . $apiPublicKey;
                 $check = json_decode(file_get_contents($checkUrl));
@@ -621,7 +628,7 @@ class PostController extends Controller
                 $hd     = 'http://img.youtube.com/vi/'.$videoId.'/hqdefault.jpg';
                 $hr     = 'http://img.youtube.com/vi/'.$videoId.'/maxresdefault.jpg';
 
-                if (count($check->items) > 0) 
+                if (count($check->items) > 0)
                 {
                     return (object)array(
                         'url' => $mediaUrl,
